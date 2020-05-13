ed0b9ef9b0c9698ddc90709169170ad4cab7a23c
navnit <navnit.chaudhary@misport.com>
2020-05-13 11:01:39 +0530

    Updates

---
+++ precache-manifest.e218b3ef607ae89379e44e755463e090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b7fa9981b80db9642c0e2575fa12e3",
+    "url": "/system-test/index.html"
+  },
+  {
+    "revision": "30738b8ef98dab2956c7",
+    "url": "/system-test/static/css/main.44ffd8dd.chunk.css"
+  },
+  {
+    "revision": "f7a70be0bc2853edf899",
+    "url": "/system-test/static/js/2.42e64a66.chunk.js"
+  },
+  {
+    "revision": "c0783ad548dc8b35d798c6b67b7a6e38",
+    "url": "/system-test/static/js/2.42e64a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30738b8ef98dab2956c7",
+    "url": "/system-test/static/js/main.043e4d8a.chunk.js"
+  },
+  {
+    "revision": "e474992e822fd0ff296e",
+    "url": "/system-test/static/js/runtime-main.2a1e3e93.js"
+  }
+]);
